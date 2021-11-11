5b6d8e2b106bb67b2e37259bc0dda00b4890284b
kwanhiuhong <hiuhongkwan@gmail.com>
2021-11-11 10:56:38 -0800

    initial commit

---
+++ src/ece/cpen502/OwnRobot.java
@@ -0,0 +1,31 @@
+package ece.cpen502;
+
+import robocode.*;
+
+public class OwnRobot extends AdvancedRobot {
+    public void run() {
+        while (true) {
+            // Replace the next 4 lines with any behavior you would like
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+            System.out.println("damn");
+        }
+    }
+    /**
+     * onScannedRobot: What to do when you see another robot
+     */
+
+    public void onScannedRobot (ScannedRobotEvent e){
+        fire(1);
+    }
+
+    /**
+     * onHitByBullet: What to do when you're hit by a bullet
+     */
+
+    public void onHitByBullet (HitByBulletEvent e){
+        turnLeft(90 - e.getBearing());
+    }
+}
